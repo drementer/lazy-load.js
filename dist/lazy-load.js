6107664be13ea3eec17acce9da78c70749fea605
Burak <48869708+drementer@users.noreply.github.com>
2022-08-01 22:24:39 +0300

    çıktı alındı

--- dist/lazy-load.js
+++ dist/lazy-load.js
@@ -1,10 +1,1 @@
-/*!
- * lazy-load.js
- * @description Sayfa yüklenme hızını artırmak için görsel içeriklerini ekranın görünür alanına yaklaşınca yükler
- * 
- * @author drementer
- * @version 1.0.3
- * @license MIT
- * @see {@link https://github.com/drementer/lazy-load.js}
- */
-document.querySelectorAll("[lazy-load]").forEach((t => { new IntersectionObserver(((t, e) => { t.forEach((t => { if (!t.isIntersecting) return; const r = t.target, o = r.getAttribute("lazy-load"); r.classList.add("yuklendi"), r.setAttribute("src", o), e.disconnect() })) }), { root: null, threshold: 1, rootMargin: "300px 0px" }).observe(t) }));
+document.querySelectorAll("[lazy-load]").forEach((t=>{new IntersectionObserver(((t,e)=>{t.forEach((t=>{if(!t.isIntersecting)return;const r=t.target,o=r.getAttribute("lazy-load");r.classList.add("yuklendi"),r.setAttribute("src",o),e.disconnect()}))}),{root:null,threshold:1,rootMargin:"300px 0px"}).observe(t)}));
