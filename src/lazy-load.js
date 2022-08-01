27d03ba9135dacbdfb09fd4a85f10ca0f0f067bc
Burak <48869708+drementer@users.noreply.github.com>
2022-08-01 22:24:34 +0300

    gereksiz yorum satırları kaldıırldı ve tr -> en camelCase isimlendirmeye geçildi

--- src/lazy-load.js
+++ src/lazy-load.js
@@ -1,54 +1,37 @@
 /*!
  * lazy-load.js
- * @description Sayfa yüklenme hızını artırmak için görsel içeriklerini ekranın görünür alanına yaklaşınca yükler
- * 
+ * @description Sayfa yüklenme hızını artırmak için görsel içeriklerini ekranın görünür alanına yaklaşınca yükler.
+ *
  * @author drementer
  * @version 1.0.3
  * @license MIT
- * @see {@link https://github.com/drementer/lazy-load.js}
+ * @link https://github.com/drementer/lazy-load.js Repository
  */
 
-/**
- * @param {Array} elemanlar lazy_load elemanlarının bulunduğu dizi
- * @param {function} io IntersectionObserver Api
- * @param {object} ayarlar io ayarları
- * @param {elemanlar[i]} eleman io'ya gönderilecek eleman
- */
-const elemanlar = document.querySelectorAll("[lazy-load]");
+const lazyLoadItem = document.querySelectorAll("[lazy-load]");
 
-elemanlar.forEach((eleman) => {
-	const ayarlar = {
+lazyLoadItem.forEach((item) => {
+	const settings = {
 		root: null,
 		threshold: 1,
 		rootMargin: "300px 0px",
 	};
 
-	/**
-	 * IntersectionObserver Api fonksiyonu
-	 *
-	 * @param {string} medya_src eleman'ın lazy-load attr'si
-	 */
 	const io = new IntersectionObserver((entries, observer) => {
-		// Eleman her ekrana girdiğinde
 		entries.forEach((entry) => {
-			// Ekrandan değilse bir şey yapma
 			if (!entry.isIntersecting) {
 				return;
 			}
 
-			// Atamalar
-			const eleman = entry.target,
-				medya_src = eleman.getAttribute("lazy-load");
+			const element = entry.target,
+				mediaLink = element.getAttribute("lazy-load");
 
-			// Ana işlev
-			eleman.classList.add("yuklendi");
-			eleman.setAttribute("src", medya_src);
+			element.classList.add("yuklendi");
+			element.setAttribute("src", mediaLink);
 
-			// İlk entry'den sonra observer'ı kapat
 			observer.disconnect();
 		});
-	}, ayarlar);
+	}, settings);
 
-	// IntersectionObserver eleman için çalıştırılıyor
-	io.observe(eleman);
+	io.observe(item);
 });
