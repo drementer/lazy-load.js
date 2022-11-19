fd0a05ca3e9a9a532b16b40a89c3d8ac1f087f16
drementer <48869708+drementer@users.noreply.github.com>
2022-11-19 13:44:42 +0300

    Deleted test folders

--- test/lazy-load.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll("[lazy-load]").forEach((t=>{new IntersectionObserver(((t,e)=>{t.forEach((t=>{if(!t.isIntersecting)return;const r=t.target,o=r.getAttribute("lazy-load");r.classList.add("yuklendi"),r.setAttribute("src",o),e.disconnect()}))}),{root:null,threshold:1,rootMargin:"300px 0px"}).observe(t)}));
