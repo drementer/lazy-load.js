c2e6f3c2b24cab1d1a8432c7574c7d83db10a874
drementer <48869708+drementer@users.noreply.github.com>
2023-11-06 21:02:20 +0300

    Fix: observer callback order

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ const lazyLoad = (selector, customSettings = {}) => {
 
   try {
     lazyItems.forEach((item) =>
-      observer(item, settings.observer, loadAsset(target, settings))
+      observer(item, loadAsset(target, settings), settings.observer)
     );
   } catch (error) {
     settings.onError(target, error.message);
