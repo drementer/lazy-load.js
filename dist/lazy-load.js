4609e14bba3316b6073f07d9ba12171c160f259a
drementer <48869708+drementer@users.noreply.github.com>
2023-11-07 00:56:08 +0300

    Build

--- dist/lazy-load.js
+++ dist/lazy-load.js
@@ -3,165 +3,125 @@
  *
  * @author drementer
  * @module lazyLoad
- * @version 1.0.6
+ * @version 1.0.7
  * @license MIT
  * @see {@link https://github.com/drementer/lazy-load.js}
- */ /**
- * Default options for lazy loading behavior.
- *
- * @type {Object}
- *
- * @param {string} [tag=selector] - The CSS selector for lazy loadable elements.
- * @param {string} [toggleClass="-loaded"] - The class name to toggle on elements after loading.
- * @param {Function} [onLoaded] - Callback function to execute when an element is successfully loaded.
- * @param {Function} [onError] - Callback function to execute when an error occurs during loading.
- * @param {Object} [observer={ root: null, threshold: 1, rootMargin: '300px 0px' }] - Configuration for IntersectionObserver used for lazy loading.
- */ const $db127449446b8099$var$options = {
-    get selector () {
-        return `[${this.tag}]`;
+ */ const $eca09eba1a2796e9$var$defaultSettings = {
+    attrs: {
+        src: "lazy",
+        srcset: "lazy-srcset",
+        poster: "lazy-poster"
     },
-    get altAttribute () {
-        return `${this.tag}-alt`;
-    },
-    tag: null,
     modifiers: {
         loaded: "-loaded",
         loading: "-loading"
     },
-    onLoaded: (element)=>{},
-    onError: (element, error)=>{
-        console.error("\uD83D\uDE80 Error on ~ element, error:", element, error);
-    },
+    onLoaded: ()=>{},
+    onLoading: ()=>{},
+    onError: (element, error)=>console.warn("Error on:", element, error),
     observer: {
         root: null,
         threshold: 1,
         rootMargin: "100% 0px"
     }
 };
-var $db127449446b8099$export$2e2bcd8739ae039 = $db127449446b8099$var$options;
+var $eca09eba1a2796e9$export$2e2bcd8739ae039 = $eca09eba1a2796e9$var$defaultSettings;
 
 
-/**
- * Loads the asset for the given image element.
- *
- * @private
- *
- * @param {HTMLImageElement} element - The image element to load the asset for.
- * @param {string} src - The asset path attribute value.
- * @param {string} alt - The asset alt attribute value.
- */ const $fa423858cf60d4ae$var$loadImage = (element, src, alt = null)=>{
-    element.src = src;
-    if (alt) element.alt = alt;
+const $fa423858cf60d4ae$var$regularLoad = (element, assets)=>{
+    element.src = assets.src;
 };
-/**
- * Loads the asset for the given picture element.
- *
- * @private
- *
- * @param {HTMLPictureElement} element - The picture element to load the asset for.
- * @param {string} src - The asset URL attribute value.
- * @param {string} alt - The asset alt attribute value.
- */ const $fa423858cf60d4ae$var$loadPicture = (element, src, alt)=>{
+const $fa423858cf60d4ae$var$loadImage = (element, assets)=>{
+    element.src = assets.src;
+    if (assets.srcset) element.srcset = assets.srcset;
+};
+const $fa423858cf60d4ae$var$loadVideo = (element, assets)=>{
+    element.src = assets.src;
+    if (assets.poster) element.poster = assets.poster;
+};
+const $fa423858cf60d4ae$var$loadPicture = (element, assets)=>{
     let img = element.querySelector("img");
     if (!img) {
         img = document.createElement("img");
-        element.append(img);
+        element.appendChild(img);
     }
-    $fa423858cf60d4ae$var$loadImage(img, src, alt);
+    $fa423858cf60d4ae$var$loadImage(img, assets);
 };
-/**
- * Loads the asset for the given video element.
- *
- * @private
- *
- * @param {HTMLVideoElement} element - The video element to load the asset for.
- * @param {string} src - The asset URL attribute value.
- */ const $fa423858cf60d4ae$var$loadVideo = (element, src)=>{
-    element.src = src;
+const $fa423858cf60d4ae$var$assetLoaders = {
+    img: $fa423858cf60d4ae$var$loadImage,
+    picture: $fa423858cf60d4ae$var$loadPicture,
+    video: $fa423858cf60d4ae$var$loadVideo,
+    iframe: $fa423858cf60d4ae$var$regularLoad,
+    embed: $fa423858cf60d4ae$var$regularLoad,
+    object: $fa423858cf60d4ae$var$regularLoad
 };
-/**
- * Loads the asset for the given element based on its type (img, picture, video).
- *
- * @private
- *
- * @param {HTMLElement} element - The element to load the asset for.
- */ const $fa423858cf60d4ae$var$loadAsset = (element, options)=>{
-    const { tag: tag, altAttr: altAttr } = options;
-    const assetLoaders = {
-        img: $fa423858cf60d4ae$var$loadImage,
-        picture: $fa423858cf60d4ae$var$loadPicture,
-        video: $fa423858cf60d4ae$var$loadVideo
+const $fa423858cf60d4ae$var$loadAsset = (element, settings)=>{
+    const elementType = element.tagName.toLowerCase();
+    const assetLoader = $fa423858cf60d4ae$var$assetLoaders[elementType];
+    if (!assetLoader) throw new Error(`Element type '${elementType}' is not supported!`);
+    const assets = {
+        src: element.getAttribute(settings.attrs.src),
+        srcset: element.getAttribute(settings.attrs.srcset),
+        poster: element.getAttribute(settings.attrs.poster)
     };
     const handleLoadEvent = ()=>{
-        element.classList.remove(options.modifiers.loading);
-        element.removeAttribute(options.tag);
-        element.classList.add(options.modifiers.loaded);
-        options.onLoaded(element);
-        element.removeEventListener("load", handleLoadEvent);
+        element.classList.remove(settings.modifiers.loading);
+        element.classList.add(settings.modifiers.loaded);
+        element.removeAttribute(settings.attrs.src);
+        element.removeAttribute(settings.attrs.srcset);
+        element.removeAttribute(settings.attrs.poster);
+        settings.onLoaded(element);
     };
-    const elementType = element.tagName.toLowerCase();
-    const assetLoader = assetLoaders[elementType];
-    const assetPath = element.getAttribute(tag);
-    const assetAltValue = element.getAttribute(altAttr);
-    assetLoader(element, assetPath, assetAltValue);
-    element.classList.add(options.modifiers.loading);
-    element.addEventListener("load", handleLoadEvent);
+    assetLoader(element, assets);
+    element.classList.add(settings.modifiers.loading);
+    element.addEventListener("load", handleLoadEvent, {
+        once: true
+    });
 };
 var $fa423858cf60d4ae$export$2e2bcd8739ae039 = $fa423858cf60d4ae$var$loadAsset;
 
 
-/**
- * Lazy load assets.
- *
- * @param {Object} [customOptions={}] - Additional options for configuring the lazy loading behavior.
- */ const $82cbb5a2f3a1bcd0$var$lazyLoad = (tag = "lazy", customOptions = {})=>{
-    (0, $db127449446b8099$export$2e2bcd8739ae039).tag = tag;
-    /**
-   * Options object for configuring the lazy loading behavior.
-   *
-   * @type {Object}
-   */ const options = {
-        ...(0, $db127449446b8099$export$2e2bcd8739ae039),
-        ...customOptions
-    };
-    /**
-   * Handles the intersection of lazy load elements.
-   *
-   * @private
-   *
-   * @param {IntersectionObserverEntry[]} entries - The entries for the intersection observer.
-   * @param {IntersectionObserver} observer - The intersection observer instance.
-   */ const handleIntersection = (entries, observer)=>{
-        const handler = (entry)=>{
-            const { target: target, isIntersecting: isIntersecting } = entry;
-            if (!isIntersecting) return;
-            try {
-                (0, $fa423858cf60d4ae$export$2e2bcd8739ae039)(target, options);
-            } catch (error) {
-                options.onError(target, error);
-            } finally{
-                observer.unobserve(target); // bunun tam testini yapmak lazim
-            }
-        };
-        entries.forEach(handler);
-    };
-    /**
-   * IntersectionObserver used for lazy loading.
-   *
-   * @type {IntersectionObserver}
-   */ const observer = new IntersectionObserver(handleIntersection, options.observer);
-    /**
-   * NodeList of lazy loadable elements.
-   *
-   * @type {NodeList}
-   */ const lazyLoadItems = document.querySelectorAll(options.selector);
-    if (!lazyLoadItems.length) {
-        console.warn("\uD83D\uDE80 No lazy loadable element found.");
-        return;
-    }
-    lazyLoadItems.forEach((item)=>observer.observe(item));
+const $5aa3f28da5200ea6$var$observer = (item, callback, settings)=>{
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            if (!entry.isIntersecting) return;
+            callback(entry.target);
+            observer.unobserve(entry.target);
+        });
+    }, settings);
+    observer.observe(item);
 };
-var $82cbb5a2f3a1bcd0$export$2e2bcd8739ae039 = $82cbb5a2f3a1bcd0$var$lazyLoad;
+var $5aa3f28da5200ea6$export$2e2bcd8739ae039 = $5aa3f28da5200ea6$var$observer;
 
 
-export {$82cbb5a2f3a1bcd0$export$2e2bcd8739ae039 as default};
+const $e5a64a9b43e3c02c$export$73eac51a39d6b0eb = (selector, root = document)=>{
+    if (selector instanceof Element) return [
+        selector
+    ];
+    if (selector instanceof NodeList) return selector;
+    if (selector instanceof Array) return selector;
+    return root.querySelectorAll(selector);
+};
+
+
+const $d582dd6eaccab18d$var$lazyLoad = (selector, customSettings = {})=>{
+    const settings = {
+        ...(0, $eca09eba1a2796e9$export$2e2bcd8739ae039),
+        ...customSettings
+    };
+    const lazyItems = (0, $e5a64a9b43e3c02c$export$73eac51a39d6b0eb)(selector);
+    if (!lazyItems.length) return console.warn("No lazy loadable element found!");
+    const observerCallback = (target)=>{
+        try {
+            settings.onLoading(target);
+            (0, $fa423858cf60d4ae$export$2e2bcd8739ae039)(target, settings);
+        } catch (error) {
+            settings.onError(target, error.message);
+        }
+    };
+    lazyItems.forEach((item)=>(0, $5aa3f28da5200ea6$export$2e2bcd8739ae039)(item, observerCallback, settings.observer));
+};
+var $d582dd6eaccab18d$export$2e2bcd8739ae039 = $d582dd6eaccab18d$var$lazyLoad;
+
+
+export {$d582dd6eaccab18d$export$2e2bcd8739ae039 as default};
