c8657482ef3c0031a0cc3c7a06e4a60b37eb3f32
drementer <48869708+drementer@users.noreply.github.com>
2024-01-17 00:05:51 +0300

    Update: Order of options

--- src/defaultOptions.js
+++ src/defaultOptions.js
@@ -8,12 +8,12 @@ export default {
     loaded: '-loaded',
     loading: '-loading',
   },
-  onLoaded: () => {},
-  onLoading: () => {},
-  onError: (element, error) => console.error('Error on:', element, error),
   observer: {
     root: null,
     threshold: 1,
     rootMargin: '100% 0px',
   },
+  onLoaded: () => {},
+  onLoading: () => {},
+  onError: (element, error) => console.error('Error on:', element, error),
 };
