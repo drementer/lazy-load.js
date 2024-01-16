9b287098598805fbddf3622d8a4b5ea1b1985bd1
drementer <48869708+drementer@users.noreply.github.com>
2024-01-17 00:14:40 +0300

    Production

--- dist/lazy-load.js
+++ dist/lazy-load.js
@@ -6,7 +6,7 @@
  * @version 1.0.7
  * @license MIT
  * @see {@link https://github.com/drementer/lazy-load.js}
- */ var $eca09eba1a2796e9$export$2e2bcd8739ae039 = {
+ */ var $caecf5182af9e868$export$2e2bcd8739ae039 = {
     attrs: {
         src: "lazy",
         srcset: "lazy-srcset",
@@ -16,56 +16,72 @@
         loaded: "-loaded",
         loading: "-loading"
     },
-    onLoaded: ()=>{},
-    onLoading: ()=>{},
-    onError: (element, error)=>console.warn("Error on:", element, error),
     observer: {
         root: null,
         threshold: 1,
         rootMargin: "100% 0px"
-    }
+    },
+    onLoaded: ()=>{},
+    onLoading: ()=>{},
+    onError: (element, error)=>console.error("Error on:", element, error)
 };
 
 
-const $fa423858cf60d4ae$var$supportedElements = [
+const $94035bc41cd26735$var$resetAssets = (element, options)=>{
+    element.removeAttribute(options.attrs.src);
+    element.removeAttribute(options.attrs.srcset);
+    element.removeAttribute(options.attrs.poster);
+};
+const $94035bc41cd26735$var$states = {
+    loading: (element, options)=>{
+        const handleLoad = ()=>$94035bc41cd26735$var$states.loaded(element, options);
+        element.classList.add(options.modifiers.loading);
+        element.addEventListener("load", handleLoad, {
+            once: true
+        });
+    },
+    loaded: (element, options)=>{
+        element.classList.remove(options.modifiers.loading);
+        element.classList.add(options.modifiers.loaded);
+        $94035bc41cd26735$var$resetAssets(element, options);
+        options.onLoaded(element);
+    },
+    error: (element, options, error)=>{
+        element.classList.remove(options.modifiers.loading);
+        options.onError(element, error);
+    }
+};
+var $94035bc41cd26735$export$2e2bcd8739ae039 = $94035bc41cd26735$var$states;
+
+
+var $fa423858cf60d4ae$export$2e2bcd8739ae039 = (element, options)=>{
+    const assets = {
+        src: element.getAttribute(options.attrs.src),
+        srcset: element.getAttribute(options.attrs.srcset),
+        poster: element.getAttribute(options.attrs.poster)
+    };
+    if (assets.src) element.setAttribute("src", assets.src);
+    if (assets.srcset) element.setAttribute("srcset", assets.srcset);
+    if (assets.poster) element.setAttribute("poster", assets.poster);
+};
+
+
+const $3557b12f3d798a99$var$supportedElements = [
     "img",
     "picture",
     "video",
     "embed",
     "object"
 ];
-const $fa423858cf60d4ae$var$loadAssets = (element, assets)=>{
-    if (assets.src) element.setAttribute("src", assets.src);
-    if (assets.srcset) element.setAttribute("srcset", assets.srcset);
-    if (assets.poster) element.setAttribute("poster", assets.poster);
-};
-const $fa423858cf60d4ae$var$elementLoaded = (element, settings)=>{
-    element.classList.add(settings.modifiers.loaded);
-    element.classList.remove(settings.modifiers.loading);
-    element.removeAttribute(settings.attrs.src);
-    element.removeAttribute(settings.attrs.srcset);
-    element.removeAttribute(settings.attrs.poster);
-    settings.onLoaded(element);
-};
-var $fa423858cf60d4ae$export$2e2bcd8739ae039 = (element, settings)=>{
+var $3557b12f3d798a99$export$2e2bcd8739ae039 = (element)=>{
     const elementType = element.tagName.toLowerCase();
-    const isSupported = $fa423858cf60d4ae$var$supportedElements.includes(elementType);
-    if (!isSupported) return console.warn("Element not supported!", element);
-    const assets = {
-        src: element.getAttribute(settings.attrs.src),
-        srcset: element.getAttribute(settings.attrs.srcset),
-        poster: element.getAttribute(settings.attrs.poster)
-    };
-    const handleLoadEvent = ()=>$fa423858cf60d4ae$var$elementLoaded(element, settings);
-    $fa423858cf60d4ae$var$loadAssets(element, assets);
-    element.classList.add(settings.modifiers.loading);
-    element.addEventListener("load", handleLoadEvent, {
-        once: true
-    });
+    const isSupported = $3557b12f3d798a99$var$supportedElements.includes(elementType);
+    if (isSupported) return true;
+    throw new Error(`Element type ${elementType} is not supported!`);
 };
 
 
-function $5aa3f28da5200ea6$export$2e2bcd8739ae039(item, callback, settings) {
+var $5aa3f28da5200ea6$export$2e2bcd8739ae039 = (item, callback, settings)=>{
     const handleIntersection = (entries, observer)=>{
         entries.forEach((entry)=>{
             if (!entry.isIntersecting) return;
@@ -75,7 +91,7 @@ function $5aa3f28da5200ea6$export$2e2bcd8739ae039(item, callback, settings) {
     };
     const observer = new IntersectionObserver(handleIntersection, settings);
     observer.observe(item);
-}
+};
 
 
 var $e5a64a9b43e3c02c$export$2e2bcd8739ae039 = (selector, root = document)=>{
@@ -88,23 +104,24 @@ var $e5a64a9b43e3c02c$export$2e2bcd8739ae039 = (selector, root = document)=>{
 };
 
 
-var $3e2aed16982f049f$export$2e2bcd8739ae039 = (selector, customSettings = {})=>{
-    const settings = {
-        ...(0, $eca09eba1a2796e9$export$2e2bcd8739ae039),
-        ...customSettings
+var $3e2aed16982f049f$export$2e2bcd8739ae039 = (selector, customOptions = {})=>{
+    const options = {
+        ...(0, $caecf5182af9e868$export$2e2bcd8739ae039),
+        ...customOptions
     };
     const lazyItems = (0, $e5a64a9b43e3c02c$export$2e2bcd8739ae039)(selector);
-    if (!lazyItems.length) return console.warn("No lazy loadable element found!");
+    if (!lazyItems.length) return console.error("No lazy loadable element found!");
     const observerCallback = (target)=>{
-        try {
-            settings.onLoading(target);
-            (0, $fa423858cf60d4ae$export$2e2bcd8739ae039)(target, settings);
-        } catch (error) {
-            settings.onError(target, error.message);
-        }
+        (0, $94035bc41cd26735$export$2e2bcd8739ae039).loading(target, options);
+        (0, $fa423858cf60d4ae$export$2e2bcd8739ae039)(target, options);
     };
     lazyItems.forEach((item)=>{
-        (0, $5aa3f28da5200ea6$export$2e2bcd8739ae039)(item, observerCallback, settings.observer);
+        try {
+            (0, $3557b12f3d798a99$export$2e2bcd8739ae039)(item);
+            (0, $5aa3f28da5200ea6$export$2e2bcd8739ae039)(item, observerCallback, options.observer);
+        } catch (error) {
+            (0, $94035bc41cd26735$export$2e2bcd8739ae039).error(item, options, error.message);
+        }
     });
 };
 
