c6ec25e02e041c0eab26639dbf53e9996a6d05af
drementer <48869708+drementer@users.noreply.github.com>
2024-01-17 19:46:00 +0300

    Refactor isSupported function to checkSupport

--- src/app.js
+++ src/app.js
@@ -11,15 +11,13 @@
 import defaultOptions from './defaultOptions.js';
 import states from './states.js';
 import loadAsset from './assetLoader.js';
-import isSupported from './isSupported.js';
+import checkSupport from './checkSupport.js';
 import observer from './observer.js';
 import getElements from './getElements.js';
 
 export default (selector, customOptions = {}) => {
   const options = { ...defaultOptions, ...customOptions };
   const lazyItems = getElements(selector);
-
-  if (!lazyItems.length) return console.error('No lazy loadable element found!');
 
   const observerCallback = (target) => {
     states.loading(target, options);
@@ -28,7 +26,7 @@ export default (selector, customOptions = {}) => {
 
   lazyItems.forEach((item) => {
     try {
-      isSupported(item);
+      checkSupport(item);
       observer(item, observerCallback, options.observer);
     } catch (error) {
       states.error(item, options, error.message);
