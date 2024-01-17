3d34e4756f55847f5df6adbf8ddeb18c2188fbbf
drementer <48869708+drementer@users.noreply.github.com>
2024-01-17 20:47:40 +0300

    Remove unsupported element check

--- src/isSupported.js
+++
@@ -1,9 +0,0 @@
-const supportedElements = ['img', 'picture', 'video', 'embed', 'object'];
-
-export default (element) => {
-  const elementType = element.tagName.toLowerCase();
-  const isSupported = supportedElements.includes(elementType);
-
-  if (isSupported) return true;
-  throw new Error(`Element type ${elementType} is not supported!`);
-};
